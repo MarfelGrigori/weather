99f7dadbde7057e3d113a7d5e4f8325b8ccfb7a2
MarfelGrigori <chief.marfel@yandex.ru>
2021-11-23 11:57:16 +0300

    fix mistake with wind

---
+++ app/src/main/java/com/example/weatherapplication/entities/WeatherWeek.kt
@@ -0,0 +1,10 @@
+package com.example.weatherapplication.entities
+
+data class WeatherWeek(
+    val time:Long,
+    val text:String,
+    val temp: Int,
+    val pressure: Int?,
+    val wind: Int
+) {
+}
