7dd8e7d5ad288ac617d66f2c1cfee13b56abb4b7
MarfelGrogori <chief.marfel@yandex.ru>
2021-11-26 14:33:25 +0300

    Change dialog window

--- app/src/main/java/com/example/weatherapplication/screens/first/FirstFragment.kt
+++ app/src/main/java/com/example/weatherapplication/screens/first/FirstFragment.kt
@@ -1,19 +1,18 @@
 package com.example.weatherapplication.screens.first
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
-import androidx.recyclerview.widget.RecyclerView
-import com.example.weatherapplication.viewModel.MainViewModel
+import com.example.weatherapplication.R
 import com.example.weatherapplication.databinding.FragmentFirstBinding
 import com.example.weatherapplication.screens.first.recyclerAdapter.WeatherWeekAdapter
 import com.example.weatherapplication.utils.changeVisibility
 import com.example.weatherapplication.utils.setPicture
+import com.example.weatherapplication.viewModel.MainViewModel
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
 
 class FirstFragment : Fragment() {
@@ -42,7 +41,16 @@ class FirstFragment : Fragment() {
         }
 //         надо исправить
         viewModel.errorBus.observe(this) {
-                MaterialAlertDialogBuilder(requireContext()).setTitle("Error").setMessage(it).show()
+            Log.e("TAG", it.toString())
+            Log.e("TAG", getString(R.string.error_network_text))
+            if (it.contains(getString(R.string.error_network_text), true)) {
+                MaterialAlertDialogBuilder(requireContext()).setTitle(R.string.error)
+                    .setMessage(getString(R.string.error_network)).show()
+            } else{
+                MaterialAlertDialogBuilder(requireContext()).setTitle(R.string.error)
+                    .setMessage(getString(R.string.something_went_wrong)).show()
+            }
+
         }
         viewModel.temperatureToday.observe(this) { binding.temperature.text = it.toString() }
         viewModel.currentCity.observe(this) {
@@ -56,6 +64,7 @@ class FirstFragment : Fragment() {
             setPicture(it, binding.image)
         }
     }
+
     override fun onDestroyView() {
         super.onDestroyView()
         binding.unbind()
