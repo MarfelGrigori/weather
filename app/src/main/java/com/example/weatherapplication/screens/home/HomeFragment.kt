bd2cf315f3bdc4a67946a33f6a19cac930787882
MarfelGrigori <chief.marfel@yandex.ru>
2021-12-09 11:36:01 +0300

    Add fastAdapter for HomeFragment

--- app/src/main/java/com/example/weatherapplication/screens/home/HomeFragment.kt
+++ app/src/main/java/com/example/weatherapplication/screens/home/HomeFragment.kt
@@ -4,38 +4,58 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.activityViewModels
 import com.example.weatherapplication.R
 import com.example.weatherapplication.databinding.FragmentFirstBinding
 import com.example.weatherapplication.di.BaseFragment
-import com.example.weatherapplication.screens.home.recyclerAdapter.WeatherWeekAdapter
+import com.example.weatherapplication.screens.home.entities.WeatherWeek
+import com.example.weatherapplication.screens.weatherday.WeatherDayFragment
+import com.example.weatherapplication.utils.Converter.getDate
 import com.example.weatherapplication.utils.changeVisibility
 import com.example.weatherapplication.utils.setPicture
 import com.example.weatherapplication.utils.toPicture
 import com.example.weatherapplication.viewModel.MainViewModel
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import com.mikepenz.fastadapter.FastAdapter
+import com.mikepenz.fastadapter.adapters.ItemAdapter
 
 class HomeFragment : BaseFragment() {
     private var _binding: FragmentFirstBinding? = null
     private val binding get() = _binding!!
     val viewModel: MainViewModel by activityViewModels { viewModelFactory }
-    lateinit var adapter: WeatherWeekAdapter
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         _binding = FragmentFirstBinding.inflate(inflater, container, false)
+
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        adapter = WeatherWeekAdapter(viewModel)
-        binding.recyclerView.adapter = adapter
+        val itemAdapter = ItemAdapter<WeatherWeek>()
+        val fastAdapter = FastAdapter.with(itemAdapter)
+        binding.recyclerView.adapter = fastAdapter
         viewModel.weatherWeek.observe(this) {
-            adapter.setItems(it)
+            itemAdapter.set(it)
         }
-
+        fastAdapter.onClickListener = { _, _, item, _ ->
+            // Handle click here
+            viewModel.date = item.time.getDate("dd/MM/yyyy")
+            val fragmentForDay = WeatherDayFragment()
+            val activity: AppCompatActivity = binding.root.context as AppCompatActivity
+            activity.supportFragmentManager.beginTransaction()
+                .replace(R.id.fragment_container_view_tag, WeatherDayFragment(),"tag")
+                .addToBackStack(null)
+                .commit()
+            val fragmentManager = fragmentForDay.activity?.supportFragmentManager
+            val fragmentTransaction = fragmentManager?.beginTransaction()
+            fragmentTransaction?.replace(R.id.fragment_container_view_tag,fragmentForDay,"tag")
+            fragmentTransaction?.commit()
+            false
+        }
         val progressBar = binding.progressBar
         viewModel.isLoading.observe(this) {
             progressBar.changeVisibility(it)
--- app/src/main/java/com/example/weatherapplication/screens/home/entities/WeatherWeek.kt
+++ app/src/main/java/com/example/weatherapplication/screens/home/entities/WeatherWeek.kt
@@ -1,10 +1,57 @@
 package com.example.weatherapplication.screens.home.entities
 
+import android.annotation.SuppressLint
+import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
+import com.example.weatherapplication.R
+import com.example.weatherapplication.utils.Converter.getDate
+import com.example.weatherapplication.utils.Converter.getDay
+import com.example.weatherapplication.utils.setPicture
+import com.example.weatherapplication.utils.toPicture
+import com.mikepenz.fastadapter.FastAdapter
+import com.mikepenz.fastadapter.items.AbstractItem
+
 data class WeatherWeek(
     val time: Long,
     val text: String,
     val temp: Int,
     val pressure: Int?,
-    val wind: Int
-)
+    val wind: Int) : AbstractItem<WeatherWeek.ViewHolder>() {
+    class ViewHolder(view: View) : FastAdapter.ViewHolder<WeatherWeek>(view) {
+        var date: TextView = view.findViewById(R.id.date)
+        var main: TextView = view.findViewById(R.id.main)
+        var wind: TextView = view.findViewById(R.id.wind)
+        var pressure: TextView = view.findViewById(R.id.pressure)
+        private var temperature: TextView = view.findViewById(R.id.temperature)
+        var image: ImageView = view.findViewById(R.id.image_)
+        var container: Any? = view.findViewById<ConstraintLayout>(R.id.item_container)
+        @SuppressLint("SetTextI18n")
+        override fun bindView(item: WeatherWeek, payloads: List<Any>) {
+          date.text = item.time.getDate("dd/MM/yyyy") + item.time.getDay()
+           main.text = item.text
+          wind.text = "wind: ${item.wind.toString()} m/s"
+          pressure.text = "pressure ${item.pressure} kPa"
+            temperature.text = "${item.temp} °C"
+            item.text.toPicture().setPicture(image)
+
+        }
+
+        override fun unbindView(item: WeatherWeek) {
+            container = null
+        }
+
+    }
+
+    override val type: Int
+        get() = R.id.item_container
+
+    override val layoutRes: Int
+        get() = R.layout.weather_item
+
+    override fun getViewHolder(v: View): ViewHolder {
+       return ViewHolder(v)
+    }
+}
 
