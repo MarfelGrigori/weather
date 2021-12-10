add4679f343496d4d89427803843a77830cc77aa
MarfelGrogori <chief.marfel@yandex.ru>
2021-12-10 16:57:27 +0300

    Change fragment test

---
+++ app/src/main/java/com/example/weatherapplication/TestActivity.kt
@@ -0,0 +1,13 @@
+package com.example.weatherapplication
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.weatherapplication.R
+
+class TestActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState, persistentState)
+        setContentView(R.layout.activity_test)
+    }
+}
--- app/src/test/java/com/example/weatherapplication/FragmentTest.kt
+++ app/src/test/java/com/example/weatherapplication/FragmentTest.kt
@@ -1,18 +1,33 @@
 package com.example.weatherapplication
 
-import androidx.core.os.bundleOf
-import androidx.fragment.app.testing.launchFragment
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.rule.ActivityTestRule
 import com.example.weatherapplication.screens.home.HomeFragment
+import junit.framework.Assert.assertNotNull
 import org.junit.Test
 import org.junit.runner.RunWith
-
+import org.robolectric.Robolectric
 
 @RunWith(AndroidJUnit4::class)
 class FragmentTest {
+
     @Test
-    fun testEventFragment() {
-        val fragmentArgs = bundleOf("numElements" to 0)
-        val scenario = launchFragment<HomeFragment>(fragmentArgs)
+    fun testLaunch() {
+        val mActivityTestRule: ActivityTestRule<TestActivity> =
+            ActivityTestRule(TestActivity::class.java)
+        var mActivity: TestActivity? = null
+        mActivity = mActivityTestRule.activity
+        Robolectric.setupActivity(mActivity!!::class.java)
+        val container =
+            mActivity.findViewById<ConstraintLayout>(R.id.test_fragment_container_view_tag)
+        val fragment = HomeFragment()
+        assertNotNull(container)
+        mActivity.supportFragmentManager.beginTransaction()
+            .replace(R.id.fragment_container_view_tag, fragment)
+            .addToBackStack(null)
+            .commitAllowingStateLoss()
+        fragment.view
+        assertNotNull(fragment)
     }
 }
