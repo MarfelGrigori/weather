1c34fdc187f7e5623f6434d3788f9578b8c9c766
MarfelGrigorij <marfelgrigorij@gmail.com>
2022-02-04 15:41:03 +0300

    Change SharedFlow in HomeViewModel.kt

--- app/src/main/java/com/example/weatherapplication/common/MainActivity.kt
+++ app/src/main/java/com/example/weatherapplication/common/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.weatherapplication.common
 
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.viewModels
 import androidx.lifecycle.ViewModelProvider
 import com.example.weatherapplication.R
@@ -11,9 +12,12 @@ import com.example.weatherapplication.common.utils.Location
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationServices
 import dagger.android.support.DaggerAppCompatActivity
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
+import kotlinx.coroutines.launch
 import javax.inject.Inject
-import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
-import io.reactivex.rxjava3.schedulers.Schedulers
+import kotlin.concurrent.thread
 
 
 class MainActivity : DaggerAppCompatActivity() {
@@ -28,14 +32,8 @@ class MainActivity : DaggerAppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         fusedLocationProvider = LocationServices.getFusedLocationProviderClient(this)
-        val location = Location(viewModel._location.first,viewModel._location.second)
+        val location = Location()
         location.getLocation(this, viewModel, viewModel1)
-            .subscribeOn(Schedulers.io())
-            .observeOn(AndroidSchedulers.mainThread())
-            .subscribe { response ->
-              location.lat = response.lat
-                location.lon = response.lon
-            }
         viewModel.loadAll()
         viewModel1.loadData()
     }
