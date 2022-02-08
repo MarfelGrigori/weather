894ae849e0bd7d0c37fd802cdedbbd760e24c294
MarfelGrigorij <marfelgrigorij@gmail.com>
2022-02-08 15:56:50 +0300

    Change code formatting

--- app/src/main/java/com/example/weatherapplication/common/MainActivity.kt
+++ app/src/main/java/com/example/weatherapplication/common/MainActivity.kt
@@ -2,7 +2,6 @@ package com.example.weatherapplication.common
 
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.viewModels
 import androidx.lifecycle.ViewModelProvider
 import com.example.weatherapplication.R
