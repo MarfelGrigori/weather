5618febb593bf16274003506ac174b06e9b6d0bb
MarfelGrigorij <marfelgrigorij@gmail.com>
2022-02-10 17:07:02 +0300

    Add cancellation in Location.kt

--- app/src/main/java/com/example/weatherapplication/common/utils/Location.kt
+++ app/src/main/java/com/example/weatherapplication/common/utils/Location.kt
@@ -6,10 +6,8 @@ import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationRequest.PRIORITY_HIGH_ACCURACY
 import com.google.android.gms.location.LocationServices
 import com.google.android.gms.tasks.CancellationTokenSource
-import com.google.android.gms.tasks.OnCompleteListener
 import com.google.android.gms.tasks.Task
 import io.reactivex.rxjava3.core.Single
-import io.reactivex.rxjava3.core.SingleEmitter
 
 data class Location(var lat: Double, var lon: Double)
 class LocationService(private val context: Context) {
@@ -27,20 +25,12 @@ class LocationService(private val context: Context) {
         currentLocationTask.addOnCompleteListener { task: Task<android.location.Location> ->
             emitter.onSuccess(Location(task.result.latitude, task.result.longitude))
         }
-        emitter.setCancellable { getOnCompleteListener(emitter) }
-    }
-}
-
-private fun getOnCompleteListener(emitter: SingleEmitter<Location>): OnCompleteListener<Void> {
-    return OnCompleteListener { task ->
-        if (!task.isSuccessful) {
-            emitter.tryOnError(
-                task.exception
-                    ?: IllegalStateException("Can't get location from FusedLocationProviderClient")
-            )
+        emitter.setCancellable {
+            cancellationTokenSource.cancel()
         }
     }
 }
+
 
 
 
